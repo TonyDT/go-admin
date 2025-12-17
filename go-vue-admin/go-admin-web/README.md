c952b1f5b6e5228221ca52310f8b6272c1388032
dongtao <dongtaocom@126.com>
2025-12-17 16:55:59 +0800

    element-plus npm install

--- go-vue-admin/go-admin-web/README.md
+++ go-vue-admin/go-admin-web/README.md
@@ -5,10 +5,14 @@
 [//]: # (安装Npm)
     # npm install
 
-[//]: # (安装router)
+[//]: # (安装router router文件夹)
     # npm install vue-router@4
     # npm install --save-dev @types/node
-[//]: # (安装pinia)
+[//]: # (安装pinia store文件夹)
     # npm install pinia
-[//]: # (持久存储插件)
+[//]: # (持久存储插件 同上)
     # npm install pinia-plugin-persistedstate
+
+[//]: # (安装element plus)
+    # npm install element-plus
+    # npm install @element-plus/icons-vue 图标
--- go-vue-admin/go-admin-web/src/main.ts
+++ go-vue-admin/go-admin-web/src/main.ts
@@ -3,8 +3,16 @@ import './style.css'
 import App from './App.vue'
 import router from "./router";
 import pinia from "./store";
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+//导入element-plus图标
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 const app = createApp(App)
 app.use(router)
 app.use(pinia)
+for(const [key,component] of Object.entries(ElementPlusIconsVue)){
+    app.component(key,component)
+}
+app.use(ElementPlus)
 app.mount('#app')
 
